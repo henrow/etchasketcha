02c6c591760a84306a96943353e4012c4c48406d
henrow <peterhenryowen@gmail.com>
2022-02-03 21:43:41 -0600

    make number form input and some styling

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 'use strict'
+
 const button = document.querySelector('button');
 const container = document.querySelector('.container');
-function populateGrid(sides) {
-    for (let i = 0; i < (sides * sides); i++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid');
-        let square = (100 / sides);
-        newDiv.style.width = `${square}%`;
-        newDiv.style.height = `${square}%`;
-        container.appendChild(newDiv);
-    }
+const sideInput = document.querySelector('#side-number');
+let currentSides = document.querySelector("#side-number").value;
 
-    const grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => {
-        grid.addEventListener('click', function(event) {
+
+populateGrid(currentSides);
+button.addEventListener('click', clear);
+sideInput.addEventListener('click', function() {
+    currentSides = sideInput.value;
+});
+
+let mousedown = false;
+document.body.onmousedown = () => mousedown = true;
+document.body.onmouseup = () => mousedown = false;
+
+function populateGrid(sides) {
+    let square = (100 / sides);
+    for (let i = 0; i < (sides * sides); i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid');
+        gridElement.style.width = `${square}%`;
+        gridElement.style.height = `${square}%`;
+        container.appendChild(gridElement);
+        gridElement.addEventListener('mousedown', function(event) {
             this.classList.add('filled');
         });
-        grid.addEventListener('mouseover', function(event) {
-            console.log(event);
-            if (event.buttons == 1 || event.buttons == 2 || event.buttons ==3) { this.classList.add('filled'); }
+        gridElement.addEventListener('mouseover', function(event) {
+            if (mousedown) { this.classList.add('filled'); }
         });
-    });
+    }
 }
-button.addEventListener('click', reset);
 
-function reset() {
+function clear() {
     const grids = document.querySelectorAll('.grid');
     grids.forEach(grid => {
         container.removeChild(grid);
     });
-    const perSide = prompt("Per side: ");
-    populateGrid(perSide);
+    populateGrid(currentSides);
     button.textContent = "RESET";
 };
